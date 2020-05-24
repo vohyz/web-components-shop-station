8020ce91bda8d03230ca94b0510d0dc83a344921
vohyz <531322336@qq.com>
2020-05-24 18:13:38 +0800

    大概是最后一次修改bug

--- js/order.js
+++ js/order.js
@@ -10,12 +10,11 @@ function itemLoad() {
 
 function init(e) {
 	e = e["errmsg"];
-	console.log(e)
-	let temp = document.createElement('div');
 	for(let i=0;i<e.length;i++) {
-		temp.innerHTML += `<order-item data='`+JSON.stringify(e[i])+`'></order-item>`;
+		let temp = document.createElement('div');
+		temp.innerHTML = `<order-item data='`+JSON.stringify(e[i])+`'></order-item>`;
+		document.querySelector(".orderContent").appendChild(temp);
 	}
-	document.querySelector(".orderContent").appendChild(temp);
 }
 itemLoad();
 
@@ -26,10 +25,9 @@ if(Orderitem!=1) {
             var templateElem = document.getElementById('OrderTemplate');
 			var content = templateElem.content.cloneNode(true);
 			var data =this.getAttribute('data');
-			console.log(data)
 			data = JSON.parse(data);
 			for(let i=0;i<data.length;i++) {
-				let doc = document.createElement('div');
+				var doc = document.createElement('div');
 				doc.className = 'little';
 				doc.innerHTML = `<span class="gameName">`+data[i][3]+`</span><span class="x">x</span><span class="quantity">`+data[i][4]+`</span>`
 				content.querySelector(".left").appendChild(doc);
